503fa315d0d07e7c9b3377054c62c8c4f6c23dee
Katie Bernard <44992593+kathleenjbernard@users.noreply.github.com>
2020-06-22 14:58:51 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,34 @@
-var Names = ["Bear", "Yeti", "Buddy Boy", "Angel Baby", "Bad Boy", "Squeaky", "Nugget Baby", "Muppet Boy", "Alarm Clock", "Pukey"];
+const button = document.querySelector("#button");
 
-var Attacks = [
-  "Hairball Launcher",
+var attackName = [
+  "Hairball Launch",
   "Gentle Paw",
-  "Bunny Kick",
-  "Sunny Nap",
+  "Cuddle Bug",
+  "4AM Alarm",
   "Shed Storm",
-  "Mommy's Boy",
+  "Neuter",
   "Bath Time",
   "Mind Control",
-  "Restless Night",
-  "Sharp Tooth"]
+  "Flea Infestation",
+  "The Vet's Office"]
+
+var attackDescription = [
+  "Discard 1 Energy in order to use this attack. Flip a coin. If heads, the opponent is now paralyzed.",
+  "Discard all energy cards attached to your opponent's active card.",
+  "Flip a coin. If heads, the opponent is now confused. If tails, this attack does nothing.",
+  "Flip 3 coins. For each heads, this attack does an additional 20 damage to the opponent. For each tails tails, this attack does damage to self.",
+  "Your opponent is now paralyzed.",
+  "Discard 2 energy attached to this card.",
+  "If your opponent is also a cat, this attack does 2x damage. If not, this attack does nothing.",
+  "Place one of your opponent's cards in the Lost Zone.",
+  "For every damage counter on this card, attach that number of energy from your deck."
+]
 
 var hitPoints = [20,30,40,50,80,100,120,150,240,350];
-var Images = 
+
+var energyCost = [1,2,3,4];
+
+var image = 
     ["url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1588980230/38941445_2145860462357240_2857798318803648512_n.jpg_vhhoym.jpg')",
               
 "url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1588980205/46270700_224719688428511_5051646799761784266_n.jpg_alzizy.jpg')",
@@ -25,58 +40,102 @@ var Images =
      
 "url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1588980179/67485422_180859879598063_6258282993558889466_n.jpg_nl8feb.jpg')",
        "url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1588979986/51442671_795274970815052_1147665457409532584_n.jpg_or8hpb.jpg')",
-   "url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1588979985/64837613_2755246124549369_3756693184816882502_n.jpg_uria6c.jpg')",
+   "url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1590172981/bearfluff_oxqxoe.png')",
               "url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1588980246/30592445_189168158371301_1592790825039822848_n.jpg_ochb2w.jpg')",
               "url('https://res.cloudinary.com/dsldlqqsb/image/upload/v1588980263/29093355_2094006350830985_8812139277723893760_n.jpg_z02ua7.jpg')"
 ]           
-          
-var Colors = [
-  "purple", "cyan", "green", "red", "fuschia", "yellow"
-]
 
-function getRandomBackgroundColor(max) {
-  var num = Math.floor(Math.random(max)*10);
-    var backgroundColor = Colors[num];
-    return backgroundColor;
+let colors = [
+  "purple", "cyan", "green", "red", "fuschia", "yellow"]
+
+ let energyHue = {
+  "purple": "hue-rotate(90deg);",
+  "cyan": "hue-rotate(60deg);",
+  "green": "hue-rotate(300deg);",
+  "red": "hue-rotate(220deg);",
+  "fuschia": "hue-rotate(180deg);",
+  "yellow": "hue-rotate(240deg);"
 }
 
-document.querySelector(".card-background").style.backgroundColor = getRandomBackgroundColor(6);
+var cardName = ["Bear", "Yeti", "Buddy Boy", "Angel Baby", "Big Muppet", "Squeaky", "Nugget Baby", "Puddle", "Alarm Clock", "Pukey"];
 
-function getRandomNum(max) {
-    var num = Math.floor(Math.random(max)*10);
-    var cardName = Names[num];
-    return cardName;
+/*for (let i = 0, cardStatNum.length < 11, i++) { */
+
+  function assignCardStat(max, objectName) {
+      let randomNum = Math.floor(Math.random(max)*10);
+      let cardStat = ((objectName)[randomNum]);
+      return cardStat;
+      }
+
+let backgroundColor;
+
+ function assignColor(num) {
+    let val = Math.floor(Math.random(6)*10);
+    let backgroundColor = ""
+      switch(val) {
+        case 1:
+          return "purple";
+          break;
+        case 2:
+          return "cyan";
+          break;
+        case 3:
+          return "green";
+          break;
+        case 4:
+          return "red";
+          break;
+        case 5:
+          return "fuschia";
+          break;
+        case 6:
+          return "yellow";
+          break;
+        default:
+          return "silver";
+    }
+   return backgroundColor;
+ }
+
+let ballColorNum;
+
+function assignBallColor(backgroundColor) {
+  let ballColorNum = "";
+  switch(backgroundColor) {
+    case "purple":
+      return 90;
+      break;
+    case "cyan":
+      return 60;
+      break;
+    case "green":
+      return 300;
+      break;
+    case "red":
+      return 220;
+      break;
+    case "fuschia":
+      return 180;
+      break;
+    case "yellow":
+      return 240;
+      break;
+    default:
+      return 0;
+      break;
+  }
+ return ballColorNum;
 }
-document.querySelector("#name").innerHTML = getRandomNum(7);
 
-function getRandomAttack1(max) {
-  var num = Math.floor(Math.random(max)*10);
-  var attackName1 = Attacks[num];
-  return attackName1;
-}
-document.querySelector("#attack1").innerHTML = getRandomAttack1(10);
+document.querySelector(".card-background").style.backgroundColor = assignColor();
+document.querySelector(".energy-balls").style.filter = "hue-rotate("+ (assignBallColor(backgroundColor)) +")deg;";
+document.querySelector("#name").innerHTML = assignCardStat(10, cardName);
+document.querySelector("#attack1").innerHTML = assignCardStat(10, attackName);  
+document.querySelector("#attack2").innerHTML = assignCardStat(10, attackName);
+document.querySelector("#attack-description2").innerHTML = assignCardStat(10, attackDescription);
+document.querySelector(".picture").style.backgroundImage = assignCardStat(10, image);
+document.querySelector("#hp").innerHTML = assignCardStat(10, hitPoints);
+document.querySelector("#attackPower1").innerHTML = assignCardStat(7, hitPoints);
+document.querySelector("#attackPower2").innerHTML = assignCardStat(7, hitPoints);
 
-function getRandomAttack2(max) {
-  var num = Math.floor(Math.random(max)*10);
-  var attackName2 = Attacks[num];
-  return attackName2;
-}
-document.querySelector("#attack2").innerHTML = getRandomAttack2(10);
-
-function getRandomHp(max) {
-    var num = Math.floor(Math.random(max)*7.77);
-    var cardHp = hitPoints[num];
-    return cardHp;
-}
-
-document.querySelector("#hp").innerHTML = getRandomHp(10);
-document.querySelector("#attackPower1").innerHTML = getRandomHp(7);
-document.querySelector("#attackPower2").innerHTML = getRandomHp(7);
-
-function getRandomImage(max) {
-    var num = Math.floor(Math.random(max)*10);
-    var image = Images[num];
-    return image;
-}
-
-document.querySelector(".picture").style.backgroundImage = getRandomImage(10);
+/*button.onClick = */
